10f201c46b849a68ec441f6a51adda97f493c149
rima <rimaramcharan@gmail.com>
2025-02-16 03:31:14 -0500

    add more files from kat

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,44 @@
-import { Navbar } from "./components/Navbar";
-import Schedule from "./components/Schedule";
-import TodaysActivity from "./components/TodaysActivity";
-import TodaysMenu from "./components/Todaysmenu";
-import WeeklyMenu from "./components/WeeklyMenu";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+import NavbarLogin from "./components/Navbar_login";
+import Login from "./components/Login";
+import Footer from "./components/Footer";
+import App_ALogin from "./App_ALogin"; // Import App_ALogin
+import AboutUs from "./components/AboutUs"; // Import AboutUs page
 
 function App() {
+  // @ts-ignore
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <main>
-      <Navbar />
-      
-      {/* Flex container for Today's Activity and Today's Menu */}
-      <div className="flex justify-between items-start w-full px-10 mt-6">
-          <TodaysActivity />
-          <TodaysMenu />
-      </div>
-      
-      <div className="flex justify-between items-start w-full px-10 mt-8">
-        <Schedule />
-        <WeeklyMenu />
-      </div>
-    </main>
+    <Router>
+      <main className="flex flex-col min-h-screen">
+        <Routes>
+          {/* Before Login */}
+          <Route
+            path="/"
+            element={
+              <>
+                <div className="w-full fixed top-0 z-10">
+                  <NavbarLogin />
+                </div>
+                <div className="flex flex-col items-center justify-center flex-grow mt-20">
+                  <Login setIsLoggedIn={setIsLoggedIn} />
+                </div>
+                <Footer />
+              </>
+            }
+          />
+
+          {/* After Login */}
+          <Route path="/app-a-login" element={<App_ALogin />} />
+
+          {/* About Us Page (Includes NavbarLogin) */}
+          <Route path="/about-us" element={<AboutUs />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
 export default App;
-
-
