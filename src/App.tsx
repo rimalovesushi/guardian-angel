33495bc85cc939c70b714669874fd32d97e10968
rima <rimaramcharan@gmail.com>
2025-02-15 19:27:03 -0500

    homepage work

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,10 @@ function App() {
         <Navbar />
       </div>
       <div>
-        <h2 className="">Today's Menu</h2>
+        <h2 className="text-3xl font-bold text-yellow-200 px-20 py-10">Today's Activity</h2>
+          <p className="text-base text-left px-20 text-gray-600">
+          *insert activity here*
+          </p>
       </div>
     </main>
   )
